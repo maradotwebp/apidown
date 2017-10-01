e40f53c11c774ec930600963b36170fdbf672fc6
Unknown <sebastian.kruemling@gmail.com>
2017-10-01 15:35:48 +0200

    add caching of request, basic test

--- test/index.test.js
+++ test/index.test.js
@@ -1,39 +1,39 @@
 const demand = require('must');
 
-
 describe('Main Entry point', function() {
-
     describe('with no auth', function() {
         const api = require('./../lib/index.js');
         const testApi = api('www.google.com');
 
         it('should be a ApiOptions function', function() {
             demand(api).must.be.a.function();
-        })
+        });
 
         it('should return an ApiObject', function() {
             demand(testApi).must.be.a.object();
-        })
+        });
 
         describe(': apiObject', function() {
-
             it('should have a fetch method', function() {
                 demand(testApi.fetch).must.be.function();
-            })
+            });
 
-            it('should return result when fetch method is called correctly', function() {
+            it('should return result when fetch method is called correctly first', function() {
                 testApi.fetch('search?q=javascript', function(err, result) {
-                    demand(result).to.be.object();
-                })
-            })
+                    testApi.fetch('search?q=csharp', function(err, result) {
+                        testApi.fetch('search?q=javascript', function(err, result) {
+                            demand(result).to.be.object();
+                        });
+                    });
+                });
+            });
 
             it('should callback with error when fetch method is called incorrectly', function() {
                 testApi.fetch('test', function(err, result) {
                     demand(err).to.be.error();
                     demand(result).to.be.undefined();
-                })
-            })
-        })
-    })
-    
-})
+                });
+            });
+        });
+    });
+});
