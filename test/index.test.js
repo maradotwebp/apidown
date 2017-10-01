0da94a109ab84062723dda9f74c41556a275aabc
Fröhlich A <froehlich.alexander@protonmail.com>
2017-10-02 00:12:22 +0200

    Rewrite test file

--- test/index.test.js
+++ test/index.test.js
@@ -1,70 +1,79 @@
 const demand = require('must');
 
-describe('Main Entry point', function() {
-    describe('with no auth', function() {
-        const api = require('./../lib/index.js');
+describe('Main Entry point', function () {
+    const api = require('./../lib/index.js');
+
+    describe('with no auth', function () {
         const testApi = api('www.google.com');
 
-        it('should be a ApiOptions function', function() {
+        it('should be a ApiOptions function', function () {
             demand(api).must.be.a.function();
         });
 
-        it('should return an ApiObject', function() {
+        it('should return an ApiObject', function () {
             demand(testApi).must.be.a.object();
         });
 
-        describe(': apiObject', function() {
-            
-            it('should have a fetch method', function() {
-                demand(testApi.fetch).must.be.a.function();
-            });
-
-            it('should return result when fetch method is called correctly', function(done) {
-                testApi.fetch('search?q=javascript', function(err, result) {
-                    demand(result).must.be.a.object();
-                    demand(result.data).must.be.a.string();
-                    done();
-                });
-            });
-
-            it('should return cached result when fetch method is called a second time', function(done) {
-                testApi.fetch('search?q=javascript', function(err, result) {
-                    demand(result).must.be.a.object();
-                    demand(result.data).must.be.a.string();
-                    demand(result.cache).must.be.true();
-                    done();
-                });
-            });
-
-            it('should callback with error when fetch method is called incorrectly', function(done) {
-                testApi.fetch('test', function(err, result) {
-                    demand(err).must.be.an.error();
-                    demand(result).must.be.undefined();
-                    done();
-                });
-            });
-
-            it("should prefer online", (done) => {
-                testApi.fetch('search?q=javascript', function(err, result) {
-                    demand(result).must.be.a.object();
-                    demand(result.data).must.be.a.string();
-                    demand(result.cache).must.be.false();
-                    done();
-                }, {preferOnline: true});
-            });
-
-            it("should prefer online and work on error", (done) => {
-                testApi.__addToCache("test", true);
-
-                testApi.fetch('test', function(err, result) {
-                    demand(err).must.be.undefined();
-                    demand(result).must.be.a.object();
-                    demand(result.cache).must.be.true();
-                    demand(result.data).must.be.true();     
-                    done();
-                }, {preferOnline: true});
-            });
-
+        it('should have a fetch method', function () {
+            demand(testApi.fetch).must.be.a.function();
         });
+
+        runTestsWithApi(testApi);
     });
 });
+
+//Runs all tests with specified API.
+function runTestsWithApi(testApi) {
+    beforeEach(function () {
+        testApi.__clearCache();
+    })
+
+    describe('with Options: {}', function () {
+        dynamicFetch({},
+            (done) => done(),
+            (done, err, result) => {
+                demand(result.cache).must.be.true();
+                done();
+            },
+            (done) => done()
+        );
+    })
+
+    describe('with Options: { preferOnline: true }', function () {
+        dynamicFetch({ preferOnline: true },
+            (done) => done(),
+            (done, err, result) => {
+                demand(result.cache).must.be.false();
+                done();
+            },
+            (done) => done()
+        );
+    })
+
+    //Fetch automatically with options, for normal, cached, and error
+    function dynamicFetch(options, cb, cacheCb, errorCb) {
+        it("should callback when fetching normally", function(done) {
+            testApi.fetch('search?q=javascript', function (err, result) {
+                demand(err).must.be.undefined();
+                demand(result.cache).must.be.false();
+                demand(result.data).must.be.truthy();
+                cb(done, err, result);
+            }, options);
+        })
+        it("should callback when fetching cached result", function(done) {
+            testApi.__addToCache('search?q=javascript', true);
+            testApi.fetch('search?q=javascript', function (err, result) {
+                demand(err).must.be.undefined();
+                demand(result.data).must.be.truthy();
+                cacheCb(done, err, result);
+            }, options);
+        })
+        it("should callback with error when cached & online not available", function(done) {
+            testApi.fetch('test', function(err, result) {
+                demand(err).must.be.error();
+                demand(result).must.be.undefined();
+                errorCb(done, err, result);
+            }, options)
+        })
+    }
+}
